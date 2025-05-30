90e85033aed0c6d1ed72519360bc31e73070fcb5
RanjithKumarR2001 <ranjithkumarckranjith@gmail.com>
2025-05-30 12:00:43 +0530

    1. Project Created

---
+++ src/main/java/com/jsp/kalikapatha/KalikapathaApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.kalikapatha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalikapathaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalikapathaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/kalikapatha/KalikapathaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.kalikapatha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalikapathaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
